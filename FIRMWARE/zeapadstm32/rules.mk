def20167b9b13f19d6e83ccd3a1459db6285fd9a
juarendra ramadhani <43043633+juarendra@users.noreply.github.com>
2025-04-27 12:54:23 +0700

    FEAT: update enable NKRO and Mouse key

--- FIRMWARE/zeapadstm32/rules.mk
+++ FIRMWARE/zeapadstm32/rules.mk
@@ -15,7 +15,7 @@ VIA_ENABLE = yes
 LT0_ENABLE = yes
 
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
